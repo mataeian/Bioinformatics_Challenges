876bcc020ae8e7d40da9372af5b37579c9809aa0
Maryam Ataeian <105380828+mataeian@users.noreply.github.com>
2025-01-29 07:10:58 -0800

    Translating RNA to Protein

---
+++ TranslatingRNAintoProtein.py
@@ -0,0 +1,44 @@
+# Problem
+# The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
+# The RNA codon table dictates the details regarding the encoding of specific codons into the amino acid alphabet.
+
+# Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+
+# Return: The protein string encoded by s
+
+with open("rosalind_prot.txt", "r") as file:
+    s = file.read().strip()
+
+codon_table = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
+def translate_rna_to_protein(s):
+    protein = []
+    # Process the RNA string in groups of 3 
+    for i in range(0, len(s), 3):
+        codon = s[i:i + 3]
+        amino_acid = codon_table.get(codon)
+        if amino_acid == 'Stop':
+            break 
+        protein.append(amino_acid)
+    
+    return ''.join(protein)
+
+with open("protein_output" ,"w") as file:
+    protein_string = translate_rna_to_protein(s)
+    file.write(protein_string)
